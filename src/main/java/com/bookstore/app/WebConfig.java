1a68e2675c22ae33f16f702b0ee10b3670c3d78c
MarcusGitAccount <pop_marcus_98@yahoo.com>
2020-01-08 01:00:27 +0200

    enabling cors

---
+++ src/main/java/com/bookstore/app/WebConfig.java
@@ -0,0 +1,22 @@
+package com.bookstore.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@Slf4j
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    log.info("Configuring application to enable corss origin calls.");
+
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET", "PUT", "POST", "DELETE");
+  }
+}
